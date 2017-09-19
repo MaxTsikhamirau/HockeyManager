e62760d564f7ead9c2351e2df575d70dae9f3ec4
Maksim Tsikhamirau - HELMES <maksim.tsikhamirau@helmes.ee>
2017-09-19 11:57:28 +0300

    made VO as they must be (one-to-one with sharead PK)

--- src/main/java/com/example/demo/tsikhamirau/controllers/PlayerConroller.java
+++ src/main/java/com/example/demo/tsikhamirau/controllers/PlayerConroller.java
@@ -1,9 +1,11 @@
 package com.example.demo.tsikhamirau.controllers;
 
 
-import com.example.demo.tsikhamirau.Repository.PlayerRepository;
+import com.example.demo.tsikhamirau.repository.PlayerRepository;
 import com.example.demo.tsikhamirau.service.IPlayerDAOService;
 import com.example.demo.tsikhamirau.valueObjects.Player;
+import com.example.demo.tsikhamirau.valueObjects.Skill;
+import com.example.demo.tsikhamirau.valueObjects.Statistics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/players/")
+@RequestMapping("/players")
 public class PlayerConroller {
 
     @Autowired
@@ -33,9 +37,17 @@ public class PlayerConroller {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Player> createPlayer(@RequestBody Player player) {
-        // System.out.println("playerDAOService = " + playerDAOService.getAll());
-        return new ResponseEntity<Player>(playerDAOService.create(player), HttpStatus.CREATED);
+    public ResponseEntity<?> createPlayer(@RequestBody Player player) {
+//        new Player.Builder().setAge(10)
+//                .setCountry("Belarus")
+//                .setName("Daniil Tsikhamirau")
+//                .setSkill(new Skill(100, 100, 100))
+//                .setStatistics(new Statistics(20, 20)).build();
+        Player newPlayer = playerRepository.save(player);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/players")
+                .buildAndExpand(newPlayer.getPlayerId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
 }
